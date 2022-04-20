72c97ebf275d3d2a95a6dfd37574334b26ceb3f5
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-04-20 11:47:37 -0300

    feat(Services): cards

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Button, Row, Col } from 'react-bootstrap';
 import pcina from '../images/pcina-700.jpg';
+import apartment from '../images/telaAp.jpeg';
+import sport from '../images/quadra.jpeg';
 
 function Services() {
   return (
@@ -12,7 +14,8 @@ function Services() {
           </Col>
         </Row>
       </Container>
-      <div id="coverd-card-services">
+
+      <div id="coverd-card-services" className="bg-0">
         <div id="card-text">
           <h1>Rede na piscina</h1>
           <p>
@@ -30,6 +33,40 @@ function Services() {
         </div>
         <div id="card-img">
           <img src={pcina} alt="pscina com tela" width="600" height="450" />
+        </div>
+      </div>
+
+      <div id="coverd-card-services">
+        <div id="card-img">
+          <img src={apartment} alt="pscina com tela" width="600" height="450" />
+        </div>
+        <div id="card-text">
+          <h1>Redes para apartamento</h1>
+          <p>Trabalhamos com o melhor item de Goiânia para garantir a você total segurança e satisfação.</p>
+          <div>
+            <Button variant="success">
+              <i className="fa-brands fa-whatsapp"></i> Solicite seu orçamento
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div id="coverd-card-services" className="bg-1">
+        <div id="card-text">
+          <h1>Redes para quadras</h1>
+          <p>
+            A Nick Redes de Proteção vem trabalhando com telas defensivas e redes defensivas, garantindo e oferecendo
+            aos seus clientes solidez e alta inovação na produção de seu material e envolvimento com a situação,
+            incluindo mão de obra própria.
+          </p>
+          <div>
+            <Button variant="success">
+              <i className="fa-brands fa-whatsapp"></i> Solicite seu orçamento
+            </Button>
+          </div>
+        </div>
+        <div id="card-img">
+          <img src={sport} alt="pscina com tela" width="600" height="450" />
         </div>
       </div>
     </>
