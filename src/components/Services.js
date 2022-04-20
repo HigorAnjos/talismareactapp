70876d64ad7eb278f8675b0d73bac6b21c988dc5
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-04-20 11:16:48 -0300

    feat(Services): template do card

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import CardServices from './CardServices';
+import pcina from '../images/pcina-700.jpg';
 
 function Services() {
   return (
-    <Container fluid="md">
-      <Row>
-        <Col id="services-title">
-          <h1>Nossos Serviços</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <CardServices
-            title="Rede na piscina"
-            image="https://github.com/HigorAnjos/talismareactapp/blob/main/src/images/pcina.jpg"
-          />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container fluid="md">
+        <Row>
+          <Col id="services-title">
+            <h1>Nossos Serviços</h1>
+          </Col>
+        </Row>
+      </Container>
+      <div id="coverd-card-services">
+        <div id="card-text">
+          <h1>Rede na piscina</h1>
+          <p>
+            A Nick Redes de Proteção vem trabalhando com telas defensivas e redes defensivas, garantindo e oferecendo
+            aos seus clientes solidez e alta inovação na produção de seu material e envolvimento com a situação,
+            incluindo mão de obra própria. específico, qualificado e preparado, quando você realmente deseja uma Pool
+            Cover Net e administrações confiáveis e de qualidade, contate os especialistas da Nick Redes, temos custos
+            implacáveis e assistência fenomenal.
+          </p>
+        </div>
+        <div id="card-img">
+          <img src={pcina} alt="pscina com tela" width="600" height="450" />
+        </div>
+      </div>
+    </>
   );
 }
 
