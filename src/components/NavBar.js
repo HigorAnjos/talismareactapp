ea1ad4d36f89b461cddc2c114c68a57abd444104
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-04-20 12:45:16 -0300

    feat(NavBar): nome talisma inserido

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">Taslismã</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
